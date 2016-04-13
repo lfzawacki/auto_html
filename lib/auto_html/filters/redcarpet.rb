e272898919d069dd01c9e3ad17fef5abf32aaf0f
Lucas Zawacki <lfzawacki@gmail.com>
2016-04-12 23:56:14 -0300

    Target _blank for redcloth

--- lib/auto_html/filters/redcarpet.rb
+++ lib/auto_html/filters/redcarpet.rb
@@ -1,4 +1,12 @@
 require 'redcarpet'
 AutoHtml.add_filter(:redcarpet).with(:renderer => Redcarpet::Render::HTML, :markdown_options => {}) do |text, options|
-  Redcarpet::Markdown.new(options[:renderer], options[:markdown_options]).render(text)
+  renderer_options = {:link_attributes => {}}
+
+  # Target _blank should be included in renderer options
+  if options[:target]
+    renderer_options[:link_attributes][:target] = options.delete(:target)
+  end
+
+  renderer = options[:renderer].new(renderer_options)
+  Redcarpet::Markdown.new(renderer, options[:markdown_options]).render(text)
 end
